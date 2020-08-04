caaf37de758fb928979ef7945289b52b10e16c5d
Romildo Alves <raj.alves96@gmail.com>
2020-08-04 17:11:59 -0300

    Exploring JSX

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 console.log("App.js is running!");
 
 // JSX - Javascript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1> Romildo Alves </h1>
+    <p> Age: 23 </p>
+    <p> Location: São Paulo, SP </p>
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
