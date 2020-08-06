139b720ea053dba2571938d8a095e9240fa0ee9b
Romildo Alves <raj.alves96@gmail.com>
2020-08-06 16:42:22 -0300

    Arrays in JSX

--- src/app.js
+++ src/app.js
@@ -35,8 +35,9 @@ const render = () => {
       <p>{app.options.length}</p>
       <button onClick={onRemoveAll}>Remove All</button>
       <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+        {app.options.map((option, index) => (
+          <li key={index}>{option}</li>
+        ))}
       </ol>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option" />
