e6a54514d04a258ba50c0b4d50ddcb12c57c9be4
Romildo Alves <raj.alves96@gmail.com>
2020-09-28 21:03:29 -0300

    Importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
-import subtract, { square, add } from "./utils.js";
-import isSenior, { isAdult, canDrink } from "./person.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// console.log("app.js is runnnig");
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
-
-console.log(isAdult(18));
-console.log(canDrink(28));
-console.log(isSenior(65));
+const template = React.createElement("p", {}, "testing 123");
+ReactDOM.render(template, document.getElementById("app"));
